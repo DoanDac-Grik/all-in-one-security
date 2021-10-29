4590214b516cafee34d8c3f96338e674288103d6
quanghuy2612 <quangquanghuy26122000@gmail.com>
2021-10-29 18:00:43 +0700

    Add post with image file - integrate froala-editor

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -1,5 +1,8 @@
+const { format } = require('morgan');
 const Blog = require('../models/Blog');
 const Category = require('../models/Category');
+const multer  = require('multer');
+const path = require('path');
 
 class BlogController {
     /*
@@ -21,6 +24,14 @@ class BlogController {
             .then(function(categories) {
                 Blog.find(condition)
                     .then(blogs => {
+                        // Remove blog.content
+                        blogs = blogs.map(blog => {
+                            blog = blog.toObject();
+                            delete blog.content;
+                            return blog;
+                        });
+
+                        // Render view
                         res.render('blog/index', {
                             'title': 'Blog',
                             'css': 'blog',
@@ -78,9 +89,74 @@ class BlogController {
     /*
         Store blog
     */
-   store(req, res, next) {
-       res.json("Create post");
-   }
+    store(req, res, next) {
+
+        const storage = multer.diskStorage({
+            destination: function(req, file, cb) {
+                cb(null, 'public/img/post/');
+            },
+        
+            // By default, multer removes file extensions so let's add them back
+            filename: function(req, file, cb) {
+                cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+            }
+        });
+
+        const upload = multer({ storage: storage }).single('image');
+
+        upload(req, res, function(err) {
+            if (req.fileValidationError) {
+                return res.send(req.fileValidationError);
+            }
+            else if (!req.file) {
+                return res.send('Please select an image to upload');
+            }
+            else if (err instanceof multer.MulterError) {
+                return res.send(err);
+            }
+            else if (err) {
+                return res.send(err);
+            }
+            
+            var formData = req.body;
+            formData.image = req.file.filename;
+
+             if (formData.submit !== "Submit") {
+                res.status(404).render("exception/404.ejs", {
+                    layout: false
+                });
+            }
+
+            const blog = new Blog(formData);
+            blog.save({})
+                .then(() => {
+                    res.redirect('/blog');
+                })  
+                .catch(next);
+        })
+
+        
+
+
+        // var formData = req.body;
+
+        // console.log(req.file);
+        // dd();
+
+        // if (formData.submit !== "Submit") {
+        //     res.status(404).render("exception/404.ejs", {
+        //         layout: false
+        //     });
+        // }
+
+        // const blog = new Blog(formData);
+        // blog.save({})
+        //     .then(() => {
+        //         res.redirect('/blog');
+        //     })  
+        //     .catch(next);
+        
+    }
 }
 
 module.exports = new BlogController();
--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.set('view engine','ejs');
 // Use static file
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use(express.urlencoded({
     extended:true
 }));
