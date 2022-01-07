aa43a826f1c670d49fecbe9150f14c19e622a178
DoanDac-Grik <18520551@gm.uit.edu.vn>
2022-01-07 12:36:49 -0500

    demo xml to json + add some comments

--- demoRunTool.js
+++ demoRunTool.js
@@ -13,37 +13,38 @@
 // // //     console.log(`stdout: ${stdout}`);
 // // // });
 
-// // // const { exec } = require("child_process");
 
-// // // exec("ping 8.8.8.8", (error, stdout, stderr) => {
-// // //     if (error) {
-// // //         console.log(`error: ${error.message}`);
-// // //         return;
-// // //     }
-// // //     if (stderr) {
-// // //         console.log(`stderr: ${stderr}`);
-// // //         return;
-// // //     }
     
-// // //     //setTimeout(function(){ console.log("Hello"); }, 3000);
-// // //     console.log(`stdout: ${stdout}`);
-// // // });
+// get filesystem module 
+const fs = require("fs"); 
+// using the readFileSync() function 
+// and passing the path to the file 
+const buffer = fs.readFileSync("/home/kali/Desktop/scan2.xml"); 
+// use the toString() method to convert 
+// Buffer into String 
+const fileContent = buffer.toString();
+// import File System Module
+
+// import xml2js Module
+const {
+
+  parseString
+
+} = require('xml2js');
 
 
-const nvt = require('node-virustotal');
-require('dotenv').config();
- const defaultTimedInstance= nvt.makeAPI().setKey(process.env.API_KEY);
-const url="https://www.npmjs.com"
-const hashed = nvt.sha256(url);
-const theSameObject = defaultTimedInstance.urlLookup(hashed, function(err, res){
-    if (err) {
-      console.log('Well, crap.');
-      console.log(err);
-      return;
-    }
-    const data = JSON.parse(res).data.attributes.last_analysis_results;
-    console.log(data)
-    return;
-  });
-  
+
+//xml data
+var xmldata = fileContent;
+
+// parsing xml data
+parseString(xmldata, function (err, results) {
+
+// parsing to json
+let data = JSON.stringify(results)
+
+// display the json data
+console.log(JSON.parse(data).OWASPZAPReport.site[0].alerts[0].alertitem);
+});
+
 
