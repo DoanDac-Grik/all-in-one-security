956143118e23811a9cfacba7ed4c768a2fe03648
quanghuy <quangquanghuy26122000@gmail.com>
2022-01-09 02:26:08 +0700

    ui check safe url(domain), loading and reduce URL

--- helpers/virusTotalCheckUrl.js
+++ helpers/virusTotalCheckUrl.js
@@ -1,44 +1,55 @@
 const nvt = require('node-virustotal');
 require('dotenv').config();
- const request = nvt.makeAPI().setKey(process.env.API_KEY);
-//const domain="trangchunhantienquocte24h.weebly.com"
+const request = nvt.makeAPI().setKey(process.env.API_KEY);
 
 
-class CheckSafeLink{
-    checkDomain(domain){
-        return new Promise((resolve, reject)=>{
+class CheckSafeLink {
+    checkDomain(domain) {
+        return new Promise((resolve, reject) => {
             request.domainLookup(domain, function (err, res) {
                 resolve(res);
-                reject(err);  
+                reject(err);
             });
         });
     }
+
+    reduceUrl(domain) {
+        const heaerUrls = [
+            'https://',
+            'http://',
+            'http://www.',
+            'https://www.',
+        ]
+        const findInclude = heaerUrls.find(heaerUrl => domain.includes(heaerUrl))
+
+        return findInclude ? (new URL(domain)).hostname.replace('www.', '') : domain;
+    }
     //Se lam sau
     //checkUrl(url)
-       
-   
-  }
+
+
+}
 module.exports = new CheckSafeLink();
 // if (err) {
-            //     console.log('Virustotal API did not work because:');
-            //     console.log(err);
-            //     return;
-            // }
-            // var road = JSON.parse(res);
-            // const rawData = road.data.attributes.last_analysis_results;
-            // let trust=0; 
-            // let distrust=0;
-            // let resultArr =[];      
-            // for (const [key, value] of Object.entries(rawData)) {           
-            //     if(value.result !== 'clean'){
-            //             resultArr.push({[key]: value.result})
-            //         distrust++;
-            //     }
-            //     else {
-            //         trust++;
-            //     }
-            // }
-            // resolve(resultArr);
-         //  console.log(resultArr);
-         //  console.log('trust: ',trust);
-          // console.log('distrust: ',distrust);
+//     console.log('Virustotal API did not work because:');
+//     console.log(err);
+//     return;
+// }
+// var road = JSON.parse(res);
+// const rawData = road.data.attributes.last_analysis_results;
+// let trust=0; 
+// let distrust=0;
+// let resultArr =[];      
+// for (const [key, value] of Object.entries(rawData)) {           
+//     if(value.result !== 'clean'){
+//             resultArr.push({[key]: value.result})
+//         distrust++;
+//     }
+//     else {
+//         trust++;
+//     }
+// }
+// resolve(resultArr);
+//  console.log(resultArr);
+//  console.log('trust: ',trust);
+// console.log('distrust: ',distrust);
