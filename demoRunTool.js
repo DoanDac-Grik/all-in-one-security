eab7fb6ad71ec1c95b063f10cb38d2908388b76a
DoanDac-Grik <18520551@gm.uit.edu.vn>
2022-01-20 10:59:09 -0500

    fix exec

--- demoRunTool.js
+++ demoRunTool.js
@@ -14,15 +14,37 @@
 // });
 
 
-var exec = require('child-process-promise').exec;
+// var exec = require('child-process-promise').exec;
  
-exec('/usr/share/zaproxy/zap.sh -cmd  -quickurl https://google.com -quickout /home/kali/Desktop/NewFolder/all-in-one-security/public/file/okzap.xml -quickprogress')
-    .then(function (result) {
-    console.log("ok ok lala")
-    })
-    .catch(function (err) {
-        console.error('ERROR: ', err);
-    });
+// exec('/usr/share/zaproxy/zap.sh -cmd  -quickurl https://google.com -quickout /home/kali/Desktop/NewFolder/all-in-one-security/public/file/okzap.xml -quickprogress')
+//     .then(function (result) {
+//     console.log("ok ok lala")
+//     })
+//     .catch(function (err) {
+//         console.error('ERROR: ', err);
+//     });
 
+const execShPromise = require("exec-sh").promise;
+const { parseString } = require('xml2js');
+const fs = require("fs");
+
+// run interactive bash shell
+const run = async () => {
+  let out;
+
+  try {
+    out = await execShPromise('/usr/share/zaproxy/zap.sh -cmd  -quickurl https://google.com -quickout /home/kali/Desktop/NewFolder/all-in-one-security/public/file/okzap2.xml -quickprogress', true);
     
+} catch (e) {
+    console.log('Error: ', e);
+    console.log('Stderr: ', e.stderr);
+    console.log('Stdout: ', e.stdout);
+
+    return e;
+  }
+
+  console.log('out: ', out.stdout, out.stderr);
+}
+
+run();   
     
